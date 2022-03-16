f7c0059184f6bd933eb1d0b51abfea0fb6005a7e
Omar Elamin <omar.elamin@diamond.ac.uk>
2022-03-16 16:46:48 +0000

    Update dependency paths to work.

--- example-config/stOdinServer.sh
+++ example-config/stOdinServer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-
 #xspress_odin --config=$SCRIPT_DIR/odin_server.cfg --logging=debug --static-path=$(SCRIPT_DIR)/static
-xspress_odin --config=$SCRIPT_DIR/odin_server.cfg
+/dls_sw/work/R3.14.12.7/support/xspress-odin/xspress-detector/control/xspress/.venvs/xspress-ZEYHa99R/bin/xspress_odin \
+	--config=$SCRIPT_DIR/odin_server.cfg
