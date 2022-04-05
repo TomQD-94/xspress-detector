660fdb05b7938eb6262a54ce321b1cb32e0cf512
Omar Elamin <omar.elamin@diamond.ac.uk>
2022-04-05 09:55:21 +0100

    parameter_tree.py: implement a parameter tree that can has verbs:   * get: for retrieving values by EPICS client.   * put: for setting values on CPP server by EPICS client.   * set:  for setting current values on CPP server to be cached by the python adaptor. Parameters validate type of values when set and put is called. And support passing arbitrary validators to be checked at runtime. ListParametors have a special method "put_element" that caches the new list value until the value is accepted by the CPP server.

---
+++ control/xspress/src/xspress_odin/parameter_tree.py
@@ -0,0 +1,213 @@
+
+import inspect
+from functools import partial, wraps
+from typing import Callable, List, Union
+
+VALIDATE = True
+
+def is_coroutine(object):
+    """
+    see: https://stackoverflow.com/questions/67020609/partial-of-a-class-coroutine-isnt-a-coroutine-why
+    """
+    while isinstance(object, partial):
+        object = object.func
+    return inspect.iscoroutinefunction(object)
+
+class ParameterException(Exception):
+    pass
+
+class NotGettableParameterException(ParameterException):
+    pass
+
+class NotPuttableParameterException(ParameterException):
+    pass
+
+class NotSettableParameterException(ParameterException):
+    pass
+
+class ParameterInterface:
+    def __init__(self, *args):
+        raise ValueError(f"{self.__class__.__name__} is an interface and cannot be instantiated")
+    def get(self): # -> Any | raises NotGettableException
+        raise NotImplementedError()
+    def set(self, value): # -> None | rasies NotSettableException
+        raise NotImplementedError()
+    async def put(self, *args): # -> bool, json_string | raises NotPuttableException
+        raise NotImplementedError()
+
+
+def is_pos(value) -> None:
+    if value < 0:
+        raise ValueError("Value must be positive")
+
+def bound_validator(_min, _max) -> Callable[[float], None]:
+    def validator(value):
+        if value <= _min or value >= _max:
+            raise ValueError(f"Value must be between {_min} and {_max}. Value = {value} was given")
+    return validator
+
+def type_validator(_type) -> Callable[[type], None]:
+    def validator(value):
+        if not isinstance(value, _type):
+            raise TypeError(f"Value {value} is not instanse of {_type}")
+    return validator
+
+def assert_gettable(func):
+    @wraps(func)
+    def wrap(*args, **kwargs):
+        _self = args[0]
+        if _self.get_cb is None:
+            raise NotGettableParameterException()
+        return func(*args, **kwargs)
+    return wrap
+
+def assert_settable(func):
+    @wraps(func)
+    def wrap(*args, **kwargs):
+        _self = args[0]
+        if _self.set_cb is None:
+            raise NotSettableParameterException()
+        return func(*args, **kwargs)
+    return wrap
+
+def assert_puttable(func):
+    @wraps(func)
+    async def wrap(*args, **kwargs):
+        _self = args[0]
+        if _self.put_cb is None:
+            raise NotPuttableParameterException()
+        return await func(*args, **kwargs)
+    return wrap
+
+def validate(func):
+    @wraps(func)
+    def wrap(*args, **kwargs):
+        _self = args[0]
+        val_args = args[1:]
+        if _self.validators and VALIDATE:
+            for validate in _self.validators:
+                validate(*val_args, **kwargs)
+        return func(*args, **kwargs)
+    return wrap
+
+def async_validate(func):
+    @wraps(func)
+    async def wrap(*args, **kwargs):
+        _self = args[0]
+        val_args = args[1:]
+        if _self.validators and VALIDATE:
+            for validate in _self.validators:
+                validate(*val_args, **kwargs)
+        return await func(*args, **kwargs)
+    return wrap
+
+
+class VirtualParameter:
+    def __init__(self, param_type, get_cb=None, set_cb=None, put_cb=None, validators: List[Callable]=None):
+        self.param_type = param_type
+        self.get_cb = get_cb
+        self.set_cb = set_cb
+        self.put_cb = put_cb
+        self.validators = [type_validator(param_type)] if validators is None else [type_validator(param_type)] + validators
+
+    @assert_gettable
+    def get(self):
+        return self.get_cb()
+
+
+    @assert_settable
+    @validate
+    def set(self, value):
+        self.set_cb(value)
+
+    @assert_puttable
+    @async_validate
+    async def put(self, value):
+        if is_coroutine(self.put_cb):
+            return await self.put_cb(value)
+        else:
+            return self.put_cb(value)
+
+
+
+class GetVirtualParameter(VirtualParameter):
+    def __init__(self, param_type, get_cb):
+        super().__init__(param_type, get_cb=get_cb)
+
+
+class GetSetVirtualParameter(VirtualParameter):
+    def __init__(self, param_type, get_cb, set_cb):
+        super().__init__(param_type, get_cb=get_cb, set_cb=set_cb)
+
+
+class PutVirtualParameter(VirtualParameter):
+    def __init__(self, param_type, put_cb, validators=None):
+        super().__init__(param_type, put_cb=put_cb, validators=validators)
+
+
+class ValueParameter(VirtualParameter):
+    def __init__(self, param_type, initial_value, put_cb=None, validators: List[Callable]=None):
+        super().__init__(param_type, get_cb=self._get, set_cb=self._set, put_cb=put_cb, validators=validators)
+        type_validator(param_type)(initial_value)
+        self.value = initial_value
+
+    def _get(self):
+        return self.value
+
+    def _set(self, value):
+        self.value = value
+
+
+class GetSetValueParameter(ValueParameter):
+    def __init__(self, param_type, initial_value):
+        super().__init__(param_type, initial_value)
+
+class ListParameter(ValueParameter):
+    def __init__(self, put_cb=None, validators: List[Callable]=None):
+        super().__init__(list, [], put_cb=put_cb, validators=validators)
+
+    async def put_element(self, index, value):
+        if self.put_cb is None:
+            raise NotPuttableParameterException()
+        temp = self.value.copy()
+        temp[index] = value
+        resp =  await self.put_cb(temp) # Raises Exception if operation was unsuccessful so we don't set self.value.
+        self.value = temp
+        return resp
+
+
+def split_path(path_str):
+    return path_str.strip("/").split("/")
+
+class XspressParameterTree:
+    def __init__(self, tree):
+        self.tree = tree
+
+    def get(self, path):
+        return self._resolve_path(path).get()
+
+    def set(self, path, value):
+        return self._resolve_path(path).set(value)
+
+    async def put(self, path, value):
+        tokens = split_path(path)
+        args = (value)
+        is_list = False
+        if tokens[-1].isdigit():
+            is_list = True
+            index = int(tokens[-1])
+            args = (index, value)
+        parameter = self._resolve_path(path)
+        if is_list:
+            return await parameter.put_element(*args)
+        else:
+            return await parameter.put(*args)
+
+    def _resolve_path(self, path: str) -> Union[VirtualParameter, ListParameter]:
+        tokens = split_path(path)
+        if tokens[-1].isdigit(): # disregard index if parameter is a ListParameter
+            tokens = tokens[:-1]
+        parameter = self.tree
+        for token in tokens:
+            parameter = parameter[token]
+        return parameter
---
+++ control/xspress/test/test_parameter_tree.py
@@ -0,0 +1,93 @@
+import pytest
+
+from xspress_odin.parameter_tree import NotPuttableParameterException, NotSettableParameterException, VirtualParameter, GetSetVirtualParameter, GetVirtualParameter, PutVirtualParameter, \
+    ValueParameter, GetSetValueParameter, ListParameter, XspressParameterTree, bound_validator, is_pos
+
+class CalledException(Exception):
+    """We'll use this exception to verify that a callback has been called"""
+    pass
+def raise_exception(ex):
+    raise ex
+
+@pytest.mark.asyncio
+async def test_get_virtual_parameter():
+    param = GetVirtualParameter(int, get_cb=lambda: 5)
+    assert param.get() == 5
+    with pytest.raises(NotSettableParameterException):
+        param.set(-3)
+    with pytest.raises(NotPuttableParameterException):
+        await param.put(-3)
+
+@pytest.mark.asyncio
+async def test_put_virtual_parameter():
+    put_callback = lambda x: 22
+    param = PutVirtualParameter(int, put_cb=put_callback, validators=[is_pos,])
+    assert 22 == await param.put(1)
+    with pytest.raises(TypeError):
+        assert 1 == await param.put("hello")
+    with pytest.raises(ValueError):
+        await param.put(-1)
+    with pytest.raises(NotSettableParameterException):
+        await param.set(-1)
+
+@pytest.mark.asyncio
+async def test_int_value_parameter():
+    param = ValueParameter(float, 0.0, put_cb=lambda: 0.0, validators=[bound_validator(0,10)])
+    assert 0.0 == param.get() 
+    param.set(1.1)
+    assert 1.1 == param.get()
+    with pytest.raises(ValueError):
+        param.set(99.9)
+    with pytest.raises(ValueError):
+        await param.put(99.9)
+
+@pytest.mark.asyncio
+async def test_str_value_parameter():
+    str_validator = lambda s: True if s.startswith("h") else raise_exception(ValueError)
+    param = ValueParameter(str, "hello", put_cb=lambda: print("hello"),validators=[str_validator])
+    assert "hello" == param.get()
+    param.set("hola")
+    assert "hola" == param.get()
+    with pytest.raises(ValueError):
+        await param.put("bonjour")
+    with pytest.raises(TypeError):
+        await param.put(1)
+
+@pytest.mark.asyncio
+async def test_list_value_parameter():
+    validate_element = lambda x: True if x < 22 else raise_exception(ValueError)
+    list_validator = lambda l: [x for x in l if validate_element(x)]
+    param = ListParameter(put_cb=lambda x: [],validators=[list_validator])
+    assert [] == param.get()
+    param.set([1,1,1,1])
+    assert [1,1,1,1] == param.get()
+    assert [] == await param.put([21])
+    with pytest.raises(ValueError):
+        await param.put([22])
+    with pytest.raises(TypeError):
+        await param.put(1)
+
+async def dummy_async_callback(x):
+    return 42 
+
+@pytest.mark.asyncio
+async def test_parameter_tree():
+    _tree = {
+        "a": {
+            "b": GetSetValueParameter(int, 3),
+            "c": ListParameter(put_cb=dummy_async_callback),
+            "d": {
+                "e": ValueParameter(str, "hello", put_cb=dummy_async_callback)
+            }
+        }
+    }
+    tree = XspressParameterTree(_tree)
+    assert 3 == tree.get("a/b")
+    tree.set("a/c", [1,2,3,4])
+    assert [1,2,3,4] == tree.get("a/c")
+    assert 42 == await tree.put("a/c/0", 100)
+    assert [100,2,3,4] == tree.get("a/c")
+    assert "hello" == tree.get("a/d/e")
+
+
+
